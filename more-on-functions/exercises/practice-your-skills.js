40a6f410cf1226358c1edd5dd7a286006c63f168
KatieNFerg <ketchersidekatie@gmail.com>
2024-04-03 11:28:52 -0500

    katies practice

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -5,6 +5,18 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
+let arr = ['Elocution', 21, 'Clean teeth', 100];
+let firstVar = arr.map(function (num) {
+    if (typeof num === "number"){
+        return num * 3;
+    } else if (typeof num === "string"){
+        return "ARRR!";
+    } else {
+        return num;
+    }
+});
+console.log(firstVar);
+
 
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
@@ -12,4 +24,4 @@ b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
 
-let arr = ['Elocution', 21, 'Clean teeth', 100];
+
