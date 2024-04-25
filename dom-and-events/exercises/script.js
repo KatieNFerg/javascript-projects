17c16a0b51e23c96b6ffd75444eef73d2efcecad
KatieNFerg <ketchersidekatie@gmail.com>
2024-04-25 13:30:42 -0500

    work in progress

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,7 +3,15 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+   button.addEventListener('click', event=>{
+    paragraph.innerHTML = 'Houston! We have liftoff!';
+   });
+    missionAbort.addEventListener("mouseover", function( event ){
+        event.target.style.backgroundColor = "red"
+    });
+    missionAbort.addEventListener("mouseout", function( event ){
+        event.target.style.backgroundColor = "";
+    });
     
 }
 
